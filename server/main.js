7d7720a025a153de94595d8dd5d9a8f77e3bd8e3
nadav <nadav.kosov@gmail.com>
2019-02-08 00:04:40 +0200

    version 1.0 includes: add team with token res add member with mongo works!!

--- server/main.js
+++ server/main.js
@@ -1,8 +1,46 @@
 var express = require('express')
 var app = express()
+var Team = require('./src/Team.js');
+var bodyParser = require('body-parser');
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.sendFile('static/home.html',{ root : __dirname})
 })
+
+/*app.use(function timeLog (req, res, next) {
+    if(req.token && isValidToken(req.token))
+        next();
+    else
+        res.sendFile('static/home.html',{ root : __dirname})
+})*/
+
+app.get('/create-team-page',function(req,res){
+    res.sendFile('static/create-team.html',{ root : __dirname})
+})
+app.get('/join-team-page',function(req,res){
+    res.sendFile('static/join-team.html',{ root : __dirname})
+})
+
+app.post('/create-team',function(req,res){
+        console.log("create-team...");
+        Team.create(req.body.name, req.body.mail,function (token) {
+            res.send("'add team' success, team token = " + token)
+        });
+})
+app.post('/add-team-member',function(req,res){
+    console.log("add-team-member...");
+    Team.addMember(req.body.token, req.body.name, req.body.mail,function () {
+        res.send("add member success")
+    });
+})
+
+
+
+
 
 app.listen(3000)
---
+++ server/src/Team.js
@@ -0,0 +1,65 @@
+
+
+var randtoken = require('rand-token'),
+    MongoClient = require('mongodb').MongoClient;
+
+
+var Team = module.exports = {
+    create : function(name,creator,cb){
+        if(Team.isValidName(name) && Team.isValidMail(creator)){
+            MongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true },function (err, client) {
+                if (err) throw err;
+                var token= Team.generateToken()
+                console.log("token: " + token)
+                var team = { name: name, creator:creator , token:token, members:{}};
+                var db = client.db("general");
+
+                db.collection("teams").insertOne(team, function(err, res) {
+                    if (err) throw err;
+                    console.log("team '" + name + "' added");
+                    client.close();
+                    cb(token)
+                });
+            })
+        }
+        else return "email or team name is not valid"
+
+    },
+    addMember: function(teamToken,memberName,memberMail,cb){
+        if(Team.isTeamExist(teamToken) && !Team.isMemberExist(memberName)){
+            MongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true },function (err, client) {
+                if (err) throw err;
+                console.log("token: " + teamToken);
+                //var member = {name: memberName, mail: memberMail};
+                //var db = client.db("general");
+                //var query = { teamToken: teamToken };
+                var col = client.db("general").collection('teams');
+                var memberNameString = `members.${memberName}`;
+                col.update({token:teamToken},{$set:{[memberNameString]:{"mail":memberMail}}},{},function(err, res){
+                    if (err) throw err;
+                    console.log("member '" + memberName + "' added");
+                    client.close();
+                    cb()
+                })
+            })
+        }
+        else return "team is not exist or member already exist"
+    },
+    generateToken: function(){
+        return randtoken.generate(8);
+    },
+    isValidName: function (name) {
+        return true
+    },
+    isValidMail: function (mail) {
+        return true
+    },
+    isTeamExist: function (teamToken) {
+        return true
+    },
+    isMemberExist: function (memberName) {
+        return false
+    }
+
+
+}
